8e8228ba77f963af282e0f5657ce8e922949a1c0
tttthomasssss <th0mas.ko6er@gmail.com>
2018-01-31 15:27:14 +0000

    little bit of cleanup

--- paralysis/parameter_ablation.py
+++ paralysis/parameter_ablation.py
@@ -1,3 +1,5 @@
+import collections
+
 from patsy import dmatrices
 from patsy import LookupFactor
 from patsy import ModelDesc
@@ -12,19 +14,26 @@ class ParameterAnalyser():
 	def __init__(self, d, **kwargs):
 		self.label_name_ = kwargs.pop('label_name', 'y')
 		self.data_ = util.load_data(d=d, **kwargs)
+		self.anova_table_ = None
+		self.anova_result_ = None
+		self.model_ = None
 
 		feat_interactions = kwargs.pop('feature_interaction_order', 1)
 		if (feat_interactions > 1):
-			self.data_ = util.create_higher_order_feature_interactions(df=self.data_, feature_interactions=feat_interactions)
+			self.data_ = util.create_higher_order_feature_interactions(
+				df=self.data_, feature_interactions=feat_interactions
+			)
 
 	def fit_ols(self, anova_type=2):
 		data_columns = self.data_.columns.values.tolist()
 
-		# Create statsmodels formula with patsy --> can't use just a simple string becasue the patsy parser fails after 485 features: https://github.com/pydata/patsy/issues/18
+		# Create statsmodels formula with patsy --> can't use just a simple string becasue the patsy parser fails after
+		# 485 features: https://github.com/pydata/patsy/issues/18
 		# http://patsy.readthedocs.io/en/latest/formulas.html
-		formula = ModelDesc([Term([LookupFactor(self.label_name_)])],  # LHS
-							[Term([LookupFactor(fn, force_categorical=True)]) for fn in data_columns if fn != self.label_name_]  # RHS
-							)
+		formula = ModelDesc(
+			[Term([LookupFactor(self.label_name_)])],  # LHS
+			[Term([LookupFactor(fn, force_categorical=True)]) for fn in data_columns if fn != self.label_name_]  # RHS
+		)
 
 		y_dm, X_dm = dmatrices(formula, self.data_, return_type='matrix')
 
@@ -40,10 +49,14 @@ class ParameterAnalyser():
 		model.model.data.design_info = X_dm.design_info
 
 		# ANOVA ablation
-		anova_table = sm.stats.anova_lm(model, typ=anova_type)  # ANOVA type explanation: https://mcfromnz.wordpress.com/2011/03/02/anova-type-iiiiii-ss-explained/
+		# ANOVA type explanation: https://mcfromnz.wordpress.com/2011/03/02/anova-type-iiiiii-ss-explained/
+		anova_table = sm.stats.anova_lm(model, typ=anova_type)
 		anova_table['pct_explained'] = (anova_table['sum_sq'] / anova_table['sum_sq'].sum()) * 100
 
-		result = {
-			'explained_variance': model.rsquared_adj,
-			'mse': model.mse_total
-		}
+		self.model_ = model
+		self.anova_table_ = anova_table
+		self.anova_result_ = collections.namedtuple('AnovaResult', ['explained_variance', 'mse'])(
+			explained_variance=model.rsquared_adj, mse=model.mse_total
+		)
+
+		return self.anova_result_
